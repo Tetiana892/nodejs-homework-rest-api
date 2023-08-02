693f418e392634a892cab28fe9464c258b1fab88
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-08-02 16:06:44 +0300

    add delete and put router

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -42,19 +42,43 @@ router.post("/", async (req, res, next) => {
     if (error) {
       throw HttpError(400, error.message);
     }
-    // const result = await contacts.addContact(req.body);
-    // res.status(201).json(result);
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
   } catch (error) {
     next(error);
   }
 });
 
-// router.delete("/:contactId", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.removeContact(id);
+    if (!result) {
+      throw HttpError(404, "Not Found");
+    }
+    res.json({
+      message: "Contact Deleted",
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-// router.put("/:contactId", async (req, res, next) => {
-//   res.json({ message: "template message" });
-// });
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = addSchema.validate(req.body);
+    if (error) {
+      throw HttpError(400, error.message);
+    }
+    const { id } = req.params;
+    const result = await contacts.updateContact(id, req.body);
+    if (!result) {
+      throw HttpError(404, "Not Found");
+    }
+    req.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
