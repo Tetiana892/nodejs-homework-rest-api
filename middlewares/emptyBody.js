e82e34b1210bb17f7647ff2e478cf6d5febaf6f4
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-08-03 18:21:46 +0300

    add checking for emptyBody

--- middlewares/emptyBody.js
+++ middlewares/emptyBody.js
@@ -1,7 +1,8 @@
 const emptyBody = (req, res, next) => {
-  if (req.body === null) {
+  if (Object.keys(req.body).length === 0) {
     return res.status(400).json({ message: "missing fields" });
   }
   next();
 };
+
 module.exports = emptyBody;
