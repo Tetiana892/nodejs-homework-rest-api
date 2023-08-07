1d3eb05bb3e7d5ec9dfe2bc25f194f93906d1be0
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-08-07 19:58:44 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const DB_HOST =
+  "mongodb+srv://Tetiana:masCgNwX7rkTyP5f@cluster0.1w2wtnl.mongodb.net/db-contacts?retryWrites=true&w=majority";
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
