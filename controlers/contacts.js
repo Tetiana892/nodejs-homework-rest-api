5744415f3cb3d789165456b1aedd7c120f2fb0b9
Tatiana <115887404+Tetiana892@users.noreply.github.com>
2023-08-09 19:06:29 +0300

    update patch metod

--- controlers/contacts.js
+++ controlers/contacts.js
@@ -23,7 +23,7 @@ const addContact = async (req, res) => {
 
 const updateStatusContact = async (req, res) => {
   const { id } = req.params;
-  const result = await Contact.findById(id, req.body, { new: true });
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
   if (!result) {
     throw HttpError(404, "Not found");
   }
